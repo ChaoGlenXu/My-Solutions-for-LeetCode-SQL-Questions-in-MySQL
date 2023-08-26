454dc05a50fa50fcbdc55cbda21cd6236ac3d337
Chao (Glen) Xu <59375616+ChaoGlenXu@users.noreply.github.com>
2023-08-26 14:22:03 -0400

    Create 1148. Article Views I.sql

---
+++ SQL 50/1148. Article Views I.sql
@@ -0,0 +1,10 @@
+#1148. Article Views I
+#Easy  
+#problem statement:   https://leetcode.com/problems/article-views-i/
+
+# Write your MySQL query statement below
+
+select distinct author_id as id
+from Views 
+where author_id = viewer_id 
+order by id ASC;
